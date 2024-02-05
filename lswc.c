9ced3840500ccc367adc3ead6d8ea8377fecc45b
TParr4196 <tparr@LAPTOP-0O0FGHKA.localdomain>
2024-02-04 16:31:53 -0800

    fork

--- lswc.c
+++ lswc.c
@@ -2,6 +2,16 @@
 #include<stdlib.h>
 #include<unistd.h>
 
-int main(){
-    printf("Testing .gitignore");
+int main(int argc, char *argv[]){
+    int pfd[2];
+    pipe(pfd);
+    pid_t pid=fork();
+    if(pid>0){
+        dup2(pfd[1],1);
+        close(pfd[0]);
+        
+    }else if(pid==0){
+        dup2(pfd[0],0);
+        close(pfd[1]);
+    }
 }
