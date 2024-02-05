297fa8380d35e5f05b3680afbab49aee141f87d4
TParr4196 <tparr@LAPTOP-0O0FGHKA.localdomain>
2024-02-04 18:30:51 -0800

    parse included

--- redirout.c
+++ redirout.c
@@ -3,8 +3,14 @@
 #include<fcntl.h>
 
 int main(int argc, char *argv[]){
+    if(argc<3){
+        printf("Please provide a file name and a command in that order with spaces in between!\n");
+        return 0;
+    }
     int fd = open(argv[1],O_WRONLY|O_CREAT|O_TRUNC,S_IRUSR);
     dup2(fd,1);
     char* *p=argv+2;
     execvp(argv[2],p);
+    printf("please provide a valid command");
+    close(fd);
 }
