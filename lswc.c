8876064e657d0381b6394d9fcc49341edcb6c7ba
TParr4196 <tparr@LAPTOP-0O0FGHKA.localdomain>
2024-02-04 17:43:19 -0800

    piping

--- lswc.c
+++ lswc.c
@@ -1,17 +1,24 @@
 #include<stdio.h>
 #include<stdlib.h>
 #include<unistd.h>
-
-int main(int argc, char *argv[]){
+//int argc, char *argv[]
+int main(){
     int pfd[2];
     pipe(pfd);
     pid_t pid=fork();
+    char* commands[100];
     if(pid>0){
         dup2(pfd[1],1);
         close(pfd[0]);
-        
+        commands[0]="ls";
+        commands[1]="-1a";
+        execlp(commands[0],"ls","-1a",(char *)NULL);
     }else if(pid==0){
         dup2(pfd[0],0);
         close(pfd[1]);
+        commands[0]="wc";
+        commands[1]="-l";
+        commands[2]=NULL;
+        execlp(commands[0],"wc","-l",(char *)NULL);
     }
 }
