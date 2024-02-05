e99bc48418bb65eeec2f46f3cafb60544dc7af99
TParr4196 <tparr@LAPTOP-0O0FGHKA.localdomain>
2024-02-04 18:18:01 -0800

    redirout no checks

--- lswc.c
+++ lswc.c
@@ -1,6 +1,7 @@
 #include<stdio.h>
 #include<stdlib.h>
 #include<unistd.h>
+
 int main(int argc, char *argv[]){
     int pfd[2];
     pipe(pfd);
--- redirout.c
+++ redirout.c
@@ -2,6 +2,9 @@
 #include<unistd.h>
 #include<fcntl.h>
 
-int main(){
-    printf("testing .gitignore again :)))))");
+int main(int argc, char *argv[]){
+    int fd = open(argv[1],O_WRONLY|O_CREAT|O_TRUNC,S_IRUSR);
+    dup2(fd,1);
+    char* *p=argv+2;
+    execvp(argv[2],p);
 }
