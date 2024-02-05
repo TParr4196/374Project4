e9c98b611599eee905098aae960c5618fdce31d3
TParr4196 <tparr@LAPTOP-0O0FGHKA.localdomain>
2024-02-04 17:48:43 -0800

    reading command line

--- lswc.c
+++ lswc.c
@@ -1,24 +1,20 @@
 #include<stdio.h>
 #include<stdlib.h>
 #include<unistd.h>
-//int argc, char *argv[]
-int main(){
+int main(int argc, char *argv[]){
     int pfd[2];
     pipe(pfd);
     pid_t pid=fork();
-    char* commands[100];
     if(pid>0){
         dup2(pfd[1],1);
         close(pfd[0]);
-        commands[0]="ls";
-        commands[1]="-1a";
-        execlp(commands[0],"ls","-1a",(char *)NULL);
+        if(argc>1){
+            execlp("ls","ls","-1a",argv[1],NULL);
+        }
+        execlp("ls","ls","-1a",NULL);
     }else if(pid==0){
         dup2(pfd[0],0);
         close(pfd[1]);
-        commands[0]="wc";
-        commands[1]="-l";
-        commands[2]=NULL;
-        execlp(commands[0],"wc","-l",(char *)NULL);
+        execlp("wc","wc","-l",NULL);
     }
 }
